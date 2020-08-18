593b6c5dd1fc4013cac037eb2617192f44a208d0
Fatima Amin <fatima.amin4official@gmail.com>
2020-08-18 14:35:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
- function wakeDog (a  ,  b ) => {
+ let wakeDog  = () => {
       const a = "Xerox"
       const b = "Border Collie"
-      expect(wakeDog(a,b)).to.equal(`Wake ${a} the ${b}`
+     return `Wake ${a} the ${b}`
     }
-    it("has a function leashDog", () => {
+    
+    let leashDog  = () => {
       const a = "Boo Radley"
       const b = "Pibble"
-      expect(leashDog(a, b)).to.equal(`Leash ${a} the ${b}`)
-    })
-    it("has a function walkToPark", () => {
+     return `Leash ${a} the ${b}` ; 
+     
+    }
+    
+    let walkToPark = () => {
       const a = "Bunny"
       const b = "Labrador"
-      expect(walkToPark(a, b)).to.equal(`Walk to the park with ${a} the ${b}`)
-    })
-    it("has a function throwFrisbee", () => {
+      return `Walk to the park with ${a} the ${b}` 
+    }
+    
+   let  throwFrisbee =  () => {
       const a = "Mary J. Blige"
       const b = "Papillon"
-      expect(throwFrisbee(a, b)).to.equal(`Throw the frisbee for ${a} the ${b}`)
-    })
-    it("has a function walkHome", () => {
+      return `Throw the frisbee for ${a} the ${b}` ;
+    } 
+    
+    let  walkHome =  () => {
       const a = "Santa Paws"
       const b = "Husky"
-      expect(walkHome(a, b)).to.equal(`Walk home with ${a} the ${b}`)
-    })
-    it("has a function unleashDog", () => {
+      return `Walk home with ${a} the ${b}`
+    }
+    
+    
+     let  unleashDog =  () => {
       const a = "Lauren"
       const b = "Golden Retriever"
-      expect(unleashDog(a, b)).to.equal(`Unleash ${a} the ${b}`)
-    })
+      return `Unleash ${a} the ${b}` ; 
+    }
+    
+    
